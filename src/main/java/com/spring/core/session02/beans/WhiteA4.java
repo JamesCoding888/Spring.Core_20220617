7c92d7b9e044bd7a98ca6ca84b55815693e22af0
student <student@BE-403->
2022-06-20 21:47:50 +0800

    SpringCore20220617

---
+++ src/main/java/com/spring/core/session02/beans/WhiteA4.java
@@ -0,0 +1,26 @@
+package com.spring.core.session02.beans;
+
+public class WhiteA4 {
+	private Integer id; // 編號
+	private Color white; // 白色
+	private Size a4; // A4 尺寸
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Color getWhite() {
+		return white;
+	}
+	public void setWhite(Color white) {
+		this.white = white;
+	}
+	public Size getA4() {
+		return a4;
+	}
+	public void setA4(Size a4) {
+		this.a4 = a4;
+	}
+	
+}
