45e423a25dc80b122d91307d9e9f94c57cc74bb0
liaojames <liaojames@localhost>
2022-07-15 21:00:29 +0800

    SpringCore

--- src/main/java/com/spring/core/sql/script.sql
+++ src/main/java/com/spring/core/sql/script.sql
@@ -89,7 +89,7 @@ from job j
 where j.eid is null;
 
 
--- 20220715 select script 
+--------------------------------- 20220715 script --------------------------------- 
 
 select e.eid, e.ename, e.age, e.createtime, 
 	   j.jid, j.jname, j.eid from emp e left join job j on j.eid = e.eid;
@@ -108,8 +108,32 @@ select j.jid, j.jname, j.eid, e.eid as emp_eid, e.ename as emp_ename, e.age as e
 	from job j left join emp e on e.eid = j.eid;
 
 	
+-- book (書籤資料), stock (庫存資料), wallet (客戶雲端錢包)
+-- 建立 book (書籤資料）表
+create table if not exists book(
+	bid integer not null auto_increment,
+	bname varchar(20) not null,
+	price integer default 0,
+	ct timestamp default current_timestamp,
+	primary key( bid )
+);
 
+-- 建立 stock(庫存資料) 表
+create table if not exists stock(
+	sid integer not null auto_increment,
+	bid integer not null, -- book 的 bid
+	amount integer default 0,
+	primary key (sid),
+	foreign key(bid) references book(bid), -- 外鍵關聯
+);
 
+-- 建立 wallet (客戶雲端錢包) 表
+create table if not exists wallet(
+	wid integer not null auto_increment,
+	wname varchar(20) not null,
+	money integer default 0,
+	primary key (wid)
+);
 
 
 
